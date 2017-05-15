2e196ddb39144bde174fd60471a1ee9eebc0c07a
Yi Wei <weiyi@papayamobile.com>
2017-05-15 17:06:33 +0800

    fix

--- Meizi/MeiziCell.m
+++ Meizi/MeiziCell.m
@@ -56,7 +56,7 @@
 
 - (void)star {
     MeiziViewController *mvc = (MeiziViewController *)[self viewController];
-    [mvc initStar];
+    [mvc tapStar];
 }
 
 - (UIViewController *)viewController {
